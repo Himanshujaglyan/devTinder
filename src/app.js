d66a81938cd1b711812e74401b3f35de71140685
Himanshu <himanshujaglyan29@gmail.com>
2025-03-23 12:38:32 -0700

    Http request methods

--- src/app.js
+++ src/app.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const app = express();
 
-app.use("/api", (req, res) => {
-    res.send("Hello Himanshu Jaglyan");
+app.get("/user", (req,res)=>{
+    res.send({"Name":"Himanshu","age":"21"})
+})
+app.post("/user",(req,res)=>{
+    console.log("This is the post request means you post your data !!")
+    res.send("Data successfully saved to the DB")
+})
+app.delete("/user",(req,res)=>{
+    res.send("Deleted succesfully");
+})
+app.use("/test", (req, res) => {
+    res.send("This is the / Route");
 });
+
+
+
 
 app.listen(3000,()=>{
     console.log("Server is successfuly listening on port 3000......")
