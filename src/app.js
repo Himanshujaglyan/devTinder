a9d3c851a3b0861e903781f4ccae401e42a33eed
Himanshu <himanshujaglyan29@gmail.com>
2025-03-24 09:14:10 -0700

    Multiple Rout Handlers

--- src/app.js
+++ src/app.js
@@ -1,21 +1,32 @@
 const express = require('express');
 const app = express();
 
-app.get("/user", (req,res)=>{
-    res.send({"Name":"Himanshu","age":"21"})
-})
-app.post("/user",(req,res)=>{
-    console.log("This is the post request means you post your data !!")
-    res.send("Data successfully saved to the DB")
-})
-app.delete("/user",(req,res)=>{
-    res.send("Deleted succesfully");
-})
-app.use("/test", (req, res) => {
-    res.send("This is the / Route");
-});
-
-
+// app.get("/user", (req,res)=>{
+//     console.log(req.query)
+//     res.send({"Name":"Himanshu","age":"21"})
+// })
+// app.post("/user",(req,res)=>{
+    
+//     res.send("Data successfully saved to the DB")
+// })
+// app.delete("/user",(req,res)=>{
+//     res.send("Deleted succesfully");
+// })
+// app.use("/test", (req, res) => {
+//     res.send("This is the / Route");
+// });
+// -------------------------------------------------------------------
+//Multiple Route Handlers
+// app.use("/test",(req,res,next)=>{
+//     //request Handler
+//     console.log("Response 1")
+//     // res.send("This is the resposnse 1")
+//     next();
+// },
+// (req,res)=>{
+//     console.log("Response 2")
+//     res.send("This is the response 2")
+// })
 
 
 app.listen(3000,()=>{
