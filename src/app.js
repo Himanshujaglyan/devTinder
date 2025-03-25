f039762f653be05a87ecc2674977ace1a0a80c0e
Himanshu <himanshujaglyan29@gmail.com>
2025-03-25 07:11:40 -0700

    Middlewares

--- src/app.js
+++ src/app.js
@@ -28,6 +28,25 @@ const app = express();
 //     res.send("This is the response 2")
 // })
 
+//Thats why we use middleware because it helps us to write less or don't repeat the code
+//So here below is the middle ware which handle authetication for both getalldata and deleteuser
+app.use("/admin",(req,res,next)=>{
+    const token = "xyz";
+    const isAuthenticate = token === "xyz";
+    if(!isAuthenticate){
+        console.log("no ")
+        res.status(401).send("Your are not a valid user!!");
+    }else{
+        next();
+    }
+})
+
+app.use("/admin/getalldata", (req,res)=>{
+    res.send("Welcome data is here!")
+})
+app.use("/admin/deleteuser", (req,res)=>{
+    res.send("Successfully deleted the user!!")
+})
 
 app.listen(3000,()=>{
     console.log("Server is successfuly listening on port 3000......")
